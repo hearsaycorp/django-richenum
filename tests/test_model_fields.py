feb741d6082c3eea9a7c9eaa1fe210bd3f8f9d8f
Dale Hui <dhui@hearsaycorp.com>
2013-09-08 23:52:07 -0700

    Adding CanonicalNameEnumField.

--- tests/test_model_fields.py
+++ tests/test_model_fields.py
@@ -70,3 +70,61 @@ def LaxIndexFieldTests(TestCase):
         m.save()
         self.assertEqual(m.num.display_name, 'uno')
         self.assertEqual(NumNode.objects.filter(num='one').count(), 1)
+
+
+class CanonicalNameFieldTests(TestCase):
+    def test_inits_with_default(self):
+        default_instance = NumNode()
+        self.assertEqual(default_instance.num_str, Number.ONE)
+
+    def test_assignment_allows_enum_values(self):
+        default_instance = NumNode()
+        default_instance.num_str = Number.TWO
+        self.assertEqual(default_instance.num_str, Number.TWO)
+
+    def test_assignment_casts_strs_to_enum_values(self):
+        default_instance = NumNode()
+        default_instance.num_str = "two"
+        self.assertEqual(default_instance.num_str, Number.TWO)
+
+    def test_fetch_casts_strs_back_to_enum_values(self):
+        m = NumNode(num_str=Number.ONE)
+        m.save()
+        display_name = NumNode.objects.get(num_str=Number.ONE).num_str.display_name
+        self.assertEqual(display_name, 'uno')
+
+    def test_queries_with_enum_values(self):
+        m = NumNode(num_str=Number.ONE)
+        m.save()
+        self.assertEqual(NumNode.objects.filter(num_str=Number.ONE).count(), 1)
+
+    def test_queries_with_strs(self):
+        m = NumNode(num_str=Number.ONE)
+        m.save()
+        self.assertEqual(NumNode.objects.filter(num_str='one').count(), 1)
+
+    def test_joins_with_enum_values(self):
+        first = NumNode()
+        first.save()
+        second = NumNode(num_str=Number.TWO, parent=first)
+        second.save()
+        num_children = NumNode.objects.filter(parent__num_str=Number.ONE).count()
+        self.assertEqual(num_children, 1)
+
+    def test_joins_with_strs(self):
+        first = NumNode()
+        first.save()
+        second = NumNode(num_str=Number.TWO, parent=first)
+        second.save()
+        num_children = NumNode.objects.filter(parent__num_str='one').count()
+        self.assertEqual(num_children, 1)
+
+    def test_default_forbids_nulls(self):
+        with self.assertRaises(IntegrityError):
+            null = NumNode(num_str=None)
+            null.save()
+
+    def test_can_allow_nulls(self):
+        null_instance = NumNode(num_str=Number.ONE, num_str_nullable=None)
+        null_instance.save()
+        self.assertIsNone(null_instance.num_str_nullable)
