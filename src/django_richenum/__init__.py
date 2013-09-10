0fc32454b400dd4c03c9bd621de8044a2b6d1130
Dale Hui <dhui@hearsaycorp.com>
2013-09-10 11:19:14 -0700

    Bump version number from 1.0.0 to 1.0.1

--- src/django_richenum/__init__.py
+++ src/django_richenum/__init__.py
@@ -6,3 +6,10 @@ __all__ = (
     'forms',
     'models',
 )
+
+
+__version__ = 'unknown'
+try:
+    __version__ = __import__('pkg_resources').get_distribution('django_richenum').version
+except Exception as e:
+    pass
