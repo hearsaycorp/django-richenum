47e9255ca90a673c4ccf6cbedadedd268a68f1d0
Akshay Shah <ashah@hearsaycorp.com>
2013-12-03 14:48:48 -0800

    Bump minor version number, add to changelog

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ class DjangoTest(TestCommand):
 
 setup(
     name='django-richenum',
-    version='1.0.2',
+    version='1.1.0',
     description='Django Enum library for python.',
     long_description=(
         open('README.rst').read() + '\n\n' +
