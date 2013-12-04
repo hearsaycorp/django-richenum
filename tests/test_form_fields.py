5c34edacd793dfcc28e45fea6910b0dd70a57abd
Akshay Shah <ashah@hearsaycorp.com>
2013-12-04 02:39:45 +0000

    Add Mutiple* form fields

--- tests/test_form_fields.py
+++ tests/test_form_fields.py
@@ -3,6 +3,8 @@ from django.utils.unittest import TestCase
 
 from django_richenum.forms import CanonicalEnumField
 from django_richenum.forms import IndexEnumField
+from django_richenum.forms import MultipleCanonicalEnumField
+from django_richenum.forms import MultipleIndexEnumField
 
 from .constants import Number, Fruit
 
@@ -106,3 +108,112 @@ class IndexEnumFieldTests(TestCase):
         choices = Number.choices()
         with self.assertRaises(ValueError):
             IndexEnumField(Number, choices=choices)
+
+
+class MultipleCanonicalForm(forms.Form):
+    num = MultipleCanonicalEnumField(Number)
+    fruit = MultipleCanonicalEnumField(Fruit)
+    fruit_optional = MultipleCanonicalEnumField(Fruit, required=False)
+    fruit_default = MultipleCanonicalEnumField(Fruit, required=False, empty_value=(Fruit.APPLE,))
+
+
+class MultiCanonicalFormTests(TestCase):
+
+    def test_cant_pass_single_vals(self):
+        form = MultipleCanonicalForm({'num': 'one', 'fruit': 'apple'})
+        self.assertFalse(form.is_valid())
+
+    def test_coerces_with_canonical_name(self):
+        form = MultipleCanonicalForm({'num': ['one', 'two'], 'fruit': ['apple', 'peach']})
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['num'], [Number.ONE, Number.TWO])
+        self.assertEqual(form.cleaned_data['fruit'], [Fruit.APPLE, Fruit.PEACH])
+
+    def test_rich_enums_validate_by_canonical_name(self):
+        # Can't pass in display name.
+        form = CanonicalForm({'num': ['one', 'two'], 'fruit': ['manzana', 'melocoton']})
+        self.assertFalse(form.is_valid())
+
+    def test_ordered_enums_validate_by_canonical_name(self):
+        # Can't pass in display name or index.
+        display_form = CanonicalForm({'num': ['uno', 'dos'], 'fruit': ['apple', 'peach']})
+        index_form = CanonicalForm({'num': [1, 2], 'fruit': ['apple', 'peach']})
+
+        self.assertFalse(display_form.is_valid())
+        self.assertFalse(index_form.is_valid())
+
+    def test_defaults_to_required(self):
+        # By default, fruit is required.
+        form = CanonicalForm({'num': ['one', 'two']})
+        self.assertFalse(form.is_valid())
+
+    def test_default_empty_value(self):
+        form = MultipleCanonicalForm({'num': ['one', 'two'], 'fruit': ['apple', 'peach']})
+        # If not required, default empty value is empty list.
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['fruit_optional'], [])
+
+    def test_can_override_default_empty_value(self):
+        form = MultipleCanonicalForm({'num': ['one', 'two'], 'fruit': ['apple', 'peach']})
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['fruit_default'], (Fruit.APPLE,))
+
+    def test_cant_pass_coerce_function(self):
+        with self.assertRaises(ValueError):
+            MultipleCanonicalEnumField(Fruit, coerce=int)
+
+    def test_cant_pass_choices(self):
+        choices = Fruit.choices()
+        with self.assertRaises(ValueError):
+            MultipleCanonicalEnumField(Fruit, choices=choices)
+
+
+class MultipleIndexForm(forms.Form):
+    num = MultipleIndexEnumField(Number)
+    num_optional = MultipleIndexEnumField(Number, required=False)
+    num_default = MultipleIndexEnumField(Number, required=False, empty_value=(Number.ONE, Number.TWO))
+
+
+class MultiIndexFormTests(TestCase):
+
+    def test_cant_pass_single_vals(self):
+        form = MultipleIndexForm({'num': 1})
+        self.assertFalse(form.is_valid())
+
+    def test_coerces_with_index(self):
+        form = MultipleIndexForm({'num': [1, 2]})
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['num'], [Number.ONE, Number.TWO])
+
+    def test_ordered_enums_validate_by_index(self):
+        # Can't pass in display name or canonical name.
+        display_form = CanonicalForm({'num': ['uno', 'dos']})
+        canonical_form = CanonicalForm({'num': ['one', 'two']})
+
+        self.assertFalse(display_form.is_valid())
+        self.assertFalse(canonical_form.is_valid())
+
+    def test_defaults_to_required(self):
+        # By default, num is required.
+        form = CanonicalForm({})
+        self.assertFalse(form.is_valid())
+
+    def test_default_empty_value(self):
+        form = MultipleIndexForm({'num': [1, 2]})
+        # If not required, default empty value is empty list.
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['num_optional'], [])
+
+    def test_can_override_default_empty_value(self):
+        form = MultipleIndexForm({'num': [1, 2]})
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['num_default'], (Number.ONE, Number.TWO))
+
+    def test_cant_pass_coerce_function(self):
+        with self.assertRaises(ValueError):
+            MultipleIndexEnumField(Number, coerce=int)
+
+    def test_cant_pass_choices(self):
+        choices = Number.choices()
+        with self.assertRaises(ValueError):
+            MultipleIndexEnumField(Number, choices=choices)
