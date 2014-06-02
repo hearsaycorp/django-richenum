a9fc350795649c8d6f969206d322e23e552dca5d
Akshay Shah <ashah@hearsaycorp.com>
2014-06-02 21:30:10 +0000

    Update metadata

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ class DjangoTest(TestCommand):
 
 setup(
     name='django-richenum',
-    version='1.2.0',
+    version='1.2.1',
     description='Django Enum library for python.',
     long_description=(
         open('README.rst').read() + '\n\n' +
