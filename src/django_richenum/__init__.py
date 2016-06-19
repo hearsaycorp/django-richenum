bf0b3cb27fa2b518fcc3f5116da0e4dbde25aae8
Dale Hui <dhui@users.noreply.github.com>
2016-06-19 00:20:25 -0700

    Remove unnecessary import of form submodule

--- src/django_richenum/__init__.py
+++ src/django_richenum/__init__.py
@@ -1,13 +1,3 @@
-import forms  # noqa
-import models  # noqa
-
-
-__all__ = (
-    'forms',
-    'models',
-)
-
-
 __version__ = 'unknown'
 try:
     __version__ = __import__('pkg_resources').get_distribution('django_richenum').version
