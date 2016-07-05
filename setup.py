95c6466b2a2840ff3139288970c506799f763728
Asher Foa <afoa@hearsaycorp.com>
2016-07-04 19:56:32 -0700

    bump version

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ class DjangoTest(TestCommand):
 
 setup(
     name='django-richenum',
-    version='2.4.0',
+    version='2.4.1',
     description='Django Enum library for python.',
     long_description=(
         open('README.rst').read() + '\n\n' +
