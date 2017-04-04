20cacb6ee1c8f2ad9877dd647c21a3b3f37a4333
Adam DePue <adepue@hearsaycorp.com>
2017-04-04 23:56:27 +0000

    Add Django 1.11 support

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ tests_require = (
 
 
 install_requires = (
-    'Django>=1.8,<1.11',
+    'Django>=1.8,<1.12',
     'richenum',
     'six',
 )
@@ -78,7 +78,7 @@ class DjangoTest(TestCommand):
 
 setup(
     name='django-richenum',
-    version='3.2.0',
+    version='3.2.1',
     description='Django Enum library for python.',
     long_description=(
         open('README.rst').read() + '\n\n' +
